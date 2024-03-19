76b03960461657d0519808dc3e5c6ccd67f0a93b
NITHEESH M <110531907+NitheeshM8@users.noreply.github.com>
2024-03-19 11:12:48 +0530

    Day 2- Navbar & Sidebar

---
+++ dreamproject/src/pages/Payment.jsx
@@ -0,0 +1,9 @@
+import '../assets/css/YourWishlist.css';
+function Payment(){
+    return (
+        <div>
+         <h1>Your Payment is Successfull</h1>   
+        </div>
+    );
+}
+export default Payment;
