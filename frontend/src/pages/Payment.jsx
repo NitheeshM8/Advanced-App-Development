4de2300b1630fe78938741d0292828fa3943e337
NITHEESH M <110531907+NitheeshM8@users.noreply.github.com>
2024-03-20 23:03:04 +0530

    Day 6

--- frontend/src/pages/Payment.jsx
+++
@@ -1,9 +0,0 @@
-import '../assets/css/YourWishlist.css';
-function Payment(){
-    return (
-        <div>
-        <h1 style={{ textAlign: 'center' }}>All Booking Payments are Successfull</h1>
-        </div>
-    );
-}
-export default Payment;
